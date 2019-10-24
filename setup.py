82d6a6c9647572be65405d2f95b7b556ce4e13df
Salih Can <salih.can@inbestme.com>
2019-10-24 23:47:04 +0300

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='hasansalihcan@gmail.com',
     license='unlicense',
     install_requires=['numpy', 'matplotlib', 'pandas', 'seaborn', 'reportlab',
-                      'pypiwin32', 'blpapi @ https://bloomberg.bintray.com/pip/simple/blpapi/blpapi-3.13.1.tar.gz',
+                      'pypiwin32', 'blpapi==3.13.1 @ https://bloomberg.bintray.com/pip/simple/blpapi/blpapi-3.13.1.tar.gz#egg=blpapi-3.13.1',
                       'pdfrw'],
     packages=['tia'],
     zip_safe=False
