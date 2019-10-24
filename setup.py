a423d940a7816ff8553fe00602b371b842b02530
Salih Can <salih.can@inbestme.com>
2019-10-24 16:26:59 +0300

    fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Salih Can',
     author_email='hasansalihcan@gmail.com',
     license='unlicense',
-    install_requires=['numpy', 'matplotlib', 'pandas', 'seabon', 'reportlab', 'pythoncom', 'win32com',
+    install_requires=['numpy', 'matplotlib', 'pandas', 'seaborn', 'reportlab', 'pythoncom', 'win32com',
                       'blpapi', 'pdfrw'],
     packages=['tia'],
     zip_safe=False
