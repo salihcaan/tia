f2a3bb8f531909029e2c659d352702ff7a1f36ac
Salih Can <salih.can@inbestme.com>
2019-10-24 16:18:16 +0300

    fix

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tia',
+    version='0.0.1',
+    description='converted via lib from python2 to python3',
+    url='git@github.com:salihcaan/tia.git',
+    author='Salih Can',
+    author_email='hasansalihcan@gmail.com',
+    license='unlicense',
+    install_requires=['numpy', 'matplotlib', 'pandas', 'seabon', 'reportlab', 'pythoncom', 'win32com',
+                      'blpapi', 'pdfrw'],
+    packages=['pat'],
+    zip_safe=False
+)
